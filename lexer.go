1ff5e8abb2ea177ec7946dba0dbd9d0c62d53dab
John Hobbs <john@velvetcache.org>
2019-03-18 17:03:33 -0500

    Step 3: Hand-rolled lexer.

--- lexer.go
+++ lexer.go
@@ -1,5 +1,12 @@
 package calculator
 
+import (
+	"bytes"
+	"errors"
+	"strconv"
+	"unicode"
+)
+
 //go:generate goyacc -c -cr -o parser.go parser.y
 
 type Result struct {
@@ -7,10 +14,80 @@ type Result struct {
 }
 
 type Operation struct {
-	Operator byte
 	Operand  int
+	Operator byte
 }
 
 type lexer struct {
 	result Result
+	err    error
+	input  []byte
+	p      int
+}
+
+// Satisfies yyLexer interface
+func (l *lexer) Error(s string) {
+	l.err = errors.New(s)
+}
+
+// Satisfies yyLexer interface
+func (l *lexer) Lex(lval *yySymType) int {
+	for b := l.next(); b != 0; b = l.next() {
+		switch {
+		case unicode.IsSpace(rune(b)):
+			continue
+		case unicode.IsDigit(rune(b)):
+			l.backup()
+			return l.scanInteger(lval)
+		case b == '+':
+			return ADDITION
+		case b == '-':
+			return SUBTRACTION
+		case b == '*':
+			return MULTIPLICATION
+		case b == '/':
+			return DIVISION
+		default:
+			return int(b)
+		}
+	}
+	return 0
+}
+
+func (l *lexer) scanInteger(lval *yySymType) int {
+	buf := bytes.NewBuffer(nil)
+	for b := l.next(); ; b = l.next() {
+		switch {
+		case unicode.IsDigit(rune(b)):
+			buf.WriteByte(b)
+		default:
+			if b != 0 {
+				l.backup()
+			}
+			i, err := strconv.Atoi(buf.String())
+			if err != nil {
+				return LexError
+			}
+			lval.int = i
+			return Integer
+		}
+	}
+}
+
+// Gets the next byte in the input.
+func (l *lexer) next() byte {
+	if l.p >= len(l.input) {
+		return 0
+	}
+	b := l.input[l.p]
+	l.p++
+	return b
+}
+
+// Seek input backwards one byte.
+func (l *lexer) backup() {
+	l.p--
+	if l.p < 0 {
+		l.p = 0
+	}
 }
---
+++ lexer_test.go
@@ -0,0 +1,88 @@
+package calculator
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	cases := []struct {
+		input  string
+		tokens []int
+	}{
+		{
+			"8 600",
+			[]int{Integer, Integer},
+		},
+		{
+			"5+6",
+			[]int{Integer, ADDITION, Integer},
+		},
+		{
+			"500-60",
+			[]int{Integer, SUBTRACTION, Integer},
+		},
+		{
+			"80*5",
+			[]int{Integer, MULTIPLICATION, Integer},
+		},
+		{
+			"90/10",
+			[]int{Integer, DIVISION, Integer},
+		},
+	}
+
+	for _, tc := range cases {
+		var (
+			lval   yySymType
+			token  int
+			tokens []int
+		)
+
+		l := &lexer{input: []byte(tc.input)}
+		for {
+			token = l.Lex(&lval)
+			if token == 0 {
+				break
+			}
+			tokens = append(tokens, token)
+			if token == LexError {
+				break
+			}
+		}
+
+		if !reflect.DeepEqual(tokens, tc.tokens) {
+			t.Errorf("Unexpected tokens for %q\n  Expect: %v\n     Got: %v", tc.input, tokensToString(tc.tokens), tokensToString(tokens))
+			return
+		}
+	}
+}
+
+func tokensToString(tkns []int) string {
+	strs := []string{}
+	for _, token := range tkns {
+		switch token {
+		case LexError:
+			strs = append(strs, "LexError")
+		case Integer:
+			strs = append(strs, "Integer")
+		case ADDITION:
+			strs = append(strs, "ADDITION")
+		case SUBTRACTION:
+			strs = append(strs, "SUBTRACTION")
+		case MULTIPLICATION:
+			strs = append(strs, "MULTIPLICATION")
+		case DIVISION:
+			strs = append(strs, "DIVISION")
+		default:
+			if token < 128 {
+				strs = append(strs, string(token))
+			} else {
+				strs = append(strs, fmt.Sprintf("UNKNOWN (%d)", token))
+			}
+		}
+	}
+	return strings.Join(strs, ", ")
+}
